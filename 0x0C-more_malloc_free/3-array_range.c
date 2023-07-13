13b6abbec07cb63b3b8953e178cd7f05c794d053
AmandaNokubonga <amandamagwaza3@gmail.com>
2023-07-13 09:59:21 -0700

    task 3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,39 +1,30 @@
 #include <stdlib.h>
+#include "main.h"
 
 /**
- * array_range - Creates an array of integers representing a
- * range of values.
+ * *array_range - creates an array of integers
+ * @min: minimum range of values stored
+ * @max: maximum range of values stored and number of elements
  *
- * @min: The minimum value of the range (inclusive).
- * @max: The maximum value of the range (inclusive).
- *
- * Return: Pointer to the array of integers representing the
- * range.
- *	Returns NULL if the range is invalid or if memory
- *	allocation fails.
+ * Return: pointer to the new array
  */
 int *array_range(int min, int max)
 {
-	int idx, *arrRng;
+	int *ptr;
+	int i, size;
 
-	/* Check if min is greater than max */
 	if (min > max)
 		return (NULL);
 
-	/* Calculate the size of the range */
-	int size = max - min + 1;
+	size = max - min + 1;
 
-	/* Allocate memory for the array representing the range */
-	arrRng = malloc(size * sizeof(int));
-	if (arrRng == NULL)
+	ptr = malloc(sizeof(int) * size);
+
+	if (ptr == NULL)
 		return (NULL);
 
-	/* Populate the array with consecutive values within the range */
-	for (idx = 0; min <= max; idx++)
-	{
-		arrRng[idx] = min;
-		min++;
-	}
+	for (i = 0; min <= max; i++)
+		ptr[i] = min++;
 
-	return (arrRng);
+	return (ptr);
 }
