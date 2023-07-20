1fa8aa109abe1b0d0b43960ead6bcfe2c1308f26
AmandaNokubonga <amandamagwaza3@gmail.com>
2023-07-20 16:29:21 -0700

    task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -4,7 +4,7 @@
  * print_all - prints anything.
  * @format: a list of types of arguments passed to the function.
  *
- * Return: no return.
+ * Return: void
  */
 void print_all(const char * const format, ...)
 {
@@ -27,24 +27,24 @@ void print_all(const char * const format, ...)
 		}
 		switch (format[i])
 		{
-		case 'c':
-			printf("%c", va_arg(valist, int)), c = 1;
-			break;
-		case 'i':
-			printf("%d", va_arg(valist, int)), c = 1;
-			break;
-		case 'f':
-			printf("%f", va_arg(valist, double)), c = 1;
-			break;
-		case 's':
-			str = va_arg(valist, char *), c = 1;
-			if (!str)
-			{
-				printf("(nil)");
+			case 'c':
+				printf("%c", va_arg(valist, int)), c = 1;
 				break;
-			}
-			printf("%s", str);
-			break;
+			case 'i':
+				printf("%d", va_arg(valist, int)), c = 1;
+				break;
+			case 'f':
+				printf("%f", va_arg(valist, double)), c = 1;
+				break;
+			case 's':
+				str = va_arg(valist, char *), c = 1;
+				if (!str)
+				{
+					printf("(nil)");
+					break;
+				}
+				printf("%s", str);
+				break;
 		} i++;
 	}
 	printf("\n"), va_end(valist);
