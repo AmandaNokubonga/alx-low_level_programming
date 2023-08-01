6c2bfdd8089a6c4bc698908262f17fd5582d9e1b
AmandaNokubonga <amandamagwaza3@gmail.com>
2023-07-31 23:57:24 -0700

    task 1

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -3,22 +3,19 @@
 /**
  * listint_len - returns the number of elements in a linked lists
  * @h: linked list of type listint_t to traverse
- * calculate number of elements in the linked list
- * Traverse the linked list until the end 0
- * Increase count for each node
- * Move to the next node
- * Return: the number of nodes
+ *
+ * Return: number of nodes
  */
 size_t listint_len(const listint_t *h)
 {
-size_t nodes = 0;
+	size_t num = 0;
 
-while (h)
-{
-nodes++;
-h = h->next;
-}
+	while (h)
+	{
+		num++;
+		h = h->next;
+	}
 
-return (nodes);
+	return (num);
 }
 
