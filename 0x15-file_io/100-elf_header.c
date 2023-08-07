79cbcee3f8ee453171d6ca7754b51c9d196b19b6
AmandaNokubonga <amandamagwaza3@gmail.com>
2023-08-07 07:25:30 -0700

    task 4

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "elf_header.h"
+
+/*
+ * main - Displays the information contained in the
+ *        ELF header at the start of an ELF file.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: 0 on success.
+ *
+ * Description: If the file is not an ELF File or
+ *              the function fails - exit code 98.
+ */
+int main(int argc, char *argv[])
+{
+    /* ... Your existing main function code */
+}
+
