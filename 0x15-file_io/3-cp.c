a728181534a4c53526149c873849484898425e54
AmandaNokubonga <amandamagwaza3@gmail.com>
2023-08-07 07:11:36 -0700

    task 4

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,36 +1,53 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[]) {
-    FILE *source_file;
-    FILE *destination_file;
-    char ch;
+/**
+ * error_file - checks if files can be opened.
+ * @file_from: file_from.
+ * @file_to: file_to.
+ * @argv: arguments vector.
+ * Return: no return.
+ */
+int main(int argc, char *argv[])
+{
+	FILE *source_file;
+	FILE *destination_file;
+	char ch;
 
-    if (argc != 3) {
-        printf("Usage: %s <source_file> <destination_file>\n", argv[0]);
-        return 1;
-    }
+	if (argc != 3)
+	{
+		printf("Usage: %s <source_file> <destination_file>\n", argv[0]);
+		return (1);
+	}
 
-    source_file = fopen(argv[1], "r");
-    if (source_file == NULL) {
-        perror("Error opening source file");
-        return 1;
-    }
+	source_file = fopen(argv[1], "r");
+	if (source_file == NULL)
+	{
+		perror("Error opening source file");
+		return (1);
+	}
 
-    destination_file = fopen(argv[2], "w");
-    if (destination_file == NULL) {
-        perror("Error opening destination file");
-        fclose(source_file);
-        return 1;
-    }
+	destination_file = fopen(argv[2], "w");
+	if (destination_file == NULL)
+	{
+		perror("Error opening destination file");
+		fclose(source_file);
+		return (1);
+	}
+/**
+ * main - check the code for Holberton School students.
+ * @argc: number of arguments.
+ * @argv: arguments vector.
+ * Return: Always 0.
+ */
+	
 
-    while ((ch = fgetc(source_file)) != EOF) {
-        fputc(ch, destination_file);
-    }
+	while ((ch = fgetc(source_file)) != EOF)
+		fputc(ch, destination_file);
 
-    printf("File copied successfully.\n");
+	printf("File copied successfully.\n");
 
-    fclose(source_file);
-    fclose(destination_file);
-    return 0;
+	fclose(source_file);
+	fclose(destination_file);
+	return (0);
 }
 
