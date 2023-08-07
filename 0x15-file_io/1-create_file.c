50dbbcf518d209c9ab84d484d9acc501976f49a0
AmandaNokubonga <amandamagwaza3@gmail.com>
2023-08-07 06:02:29 -0700

    task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * create_file - function that creates a file
+ * @filename: the filename.
+ * @text_content: the name content writed in the file.
+ *
+ * Return: 1 on success. -1 on failure.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int rwr;
+	int nletters;
+	int fd;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (fd == -1)
+		return (-1);
+
+	if (!text_content)
+		text_content = "";
+
+	for (nletters = 0; text_content[nletters]; nletters++)
+		;
+
+	rwr = write(fd, text_content, nletters);
+
+	if (rwr == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
+}
