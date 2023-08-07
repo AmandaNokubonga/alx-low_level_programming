6cb29e98c1383104bed17eac5479a0f173bf4c44
AmandaNokubonga <amandamagwaza3@gmail.com>
2023-08-07 12:50:06 -0700

    task 4

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,20 +1,18 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "elf_header.h"
+#ifndef ELF_HEADER_H
+#define ELF_HEADER_H
 
-/*
- * main - Displays the information contained in the
- *        ELF header at the start of an ELF file.
- * @argc: The number of arguments supplied to the program.
- * @argv: An array of pointers to the arguments.
- *
- * Return: 0 on success.
- *
- * Description: If the file is not an ELF File or
- *              the function fails - exit code 98.
- */
-int main(int argc, char *argv[])
-{
-    /* ... Your existing main function code */
-}
+#include <elf.h>
+
+void check_elf(unsigned char *e_ident);
+void print_magic(unsigned char *e_ident);
+void print_class(unsigned char *e_ident);
+void print_data(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+void print_abi(unsigned char *e_ident);
+void print_osabi(unsigned char *e_ident);
+void print_type(unsigned int e_type, unsigned char *e_ident);
+void print_entry(unsigned long int e_entry, unsigned char *e_ident);
+void close_elf(int elf);
+
+#endif /* ELF_HEADER_H */
 
